8cb7f2e7992f0923ceb0057c343b84d41feb25a3
mehrdud <mehrdud@gmail.com>
2018-06-18 21:45:41 +0100

    First Draft

---
+++ actorsystem/src/main/java/com/piratesofpersia/actorsystem/ActorsystemApplication.java
@@ -0,0 +1,12 @@
+package com.piratesofpersia.actorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorsystemApplication.class, args);
+	}
+}
